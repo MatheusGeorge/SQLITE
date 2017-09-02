b6d96195383273392eeaa7d890a2c05a3446b45b
Matheus Vieira <matheus.vs2015@hotmail.com>
2017-09-02 05:52:38 -0300

    5

--- app/src/main/java/com/example/decar/todos/TodoListActivity.java
+++ app/src/main/java/com/example/decar/todos/TodoListActivity.java
@@ -1,18 +1,25 @@
 package com.example.decar.todos;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.example.decar.todos.data.DatabaseHelper;
+import com.example.decar.todos.data.TodosContract;
 
 public class TodoListActivity extends AppCompatActivity {
 
@@ -23,17 +30,66 @@ public class TodoListActivity extends AppCompatActivity {
             "Running session at 19.30",
             "Call Uncle Sam"
     };
+    private void readData(){
+        DatabaseHelper helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] projection = {TodosContract.TodosEntry.COLUMN_TEXT,
+                               TodosContract.TodosEntry.COLUMN_CREATED,
+                               TodosContract.TodosEntry.COLUMN_EXPIRED,
+                               TodosContract.TodosEntry.COLUMN_DONE,
+                               TodosContract.TodosEntry.COLUMN_CATEGORY};
+        String selection = TodosContract.TodosEntry.COLUMN_CATEGORY + " = ?";
+        String[] selectionArgs = {"1"};
+        Cursor c = db.query(TodosContract.TodosEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, null);
+        int i = c.getCount();
+        Log.d("Record Count ", String.valueOf(i));
+        String rowContent = "";
+        while(c.moveToNext()){
+            for(i=0; i<=4; i++){
+                rowContent += c.getString(i) + " - ";
+            }
+            Log.i("Row " + String.valueOf(c.getPosition()), rowContent);
+            rowContent = "";
+        }
+        c.close();
+    }
+    private void updateTodo() {
+        int id = 1;
+        DatabaseHelper helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] args = {String.valueOf(id)};
+        ContentValues values = new ContentValues();
+        values.put(TodosContract.TodosEntry.COLUMN_TEXT, "Call Mr Bond");
+        int numRows = db.update(TodosContract.TodosEntry.TABLE_NAME, values, TodosContract.TodosEntry._ID + " =?", args);
+        Log.d("Update Rows", String.valueOf(numRows));
+        db.close();
+
+    }
+    private void deleteTodo(){
+        int id = 1;
+        DatabaseHelper helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] args = {String.valueOf(id)};
+        int numRows = db.delete(TodosContract.TodosEntry.TABLE_NAME, TodosContract.TodosEntry._ID + " =?", args);
+        Log.d("Delete Rows", String.valueOf(numRows));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //DatabaseHelper helper = new DatabaseHelper(this);
+        //SQLiteDatabase db = helper.getReadableDatabase();
+        //CreateTodo();
+        //readData();
+        updateTodo();
+        deleteTodo();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final ListView lv=(ListView) findViewById(R.id.lvTodos);
-//adds the custom layout
+        //adds the custom layout
         lv.setAdapter(new ArrayAdapter<String>(this, R.layout.todo_list_item,
                 R.id.tvNote,itemname));
-//adds the click event to the listView, reading the content
+        //adds the click event to the listView, reading the content
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
@@ -52,6 +108,28 @@ public class TodoListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+    }
+
+    private void CreateTodo(){
+        DatabaseHelper helper = new DatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String query = "INSERT INTO todos ("
+                + TodosContract.TodosEntry.COLUMN_TEXT + ","
+                + TodosContract.TodosEntry.COLUMN_CATEGORY + ","
+                + TodosContract.TodosEntry.COLUMN_CREATED + ","
+                + TodosContract.TodosEntry.COLUMN_EXPIRED + ","
+                + TodosContract.TodosEntry.COLUMN_DONE + ")"
+                + " VALUES (\"Go to the gym\", 1, \"2016-01-01\", \"\", 0)";
+        db.execSQL(query);
+
+        ContentValues values = new ContentValues();
+        values.put(TodosContract.TodosEntry.COLUMN_TEXT, "Call Mr Bean");
+        values.put(TodosContract.TodosEntry.COLUMN_CATEGORY, 1);
+        values.put(TodosContract.TodosEntry.COLUMN_CREATED, "2016-01-02");
+        values.put(TodosContract.TodosEntry.COLUMN_DONE, 0);
+        long todo_id = db.insert(TodosContract.TodosEntry.TABLE_NAME, null, values);
+
 
     }
 }
